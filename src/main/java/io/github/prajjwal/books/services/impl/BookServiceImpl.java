503a118ea7969d684e414f606308b5ea657115fd
Prajjwal <prajjwalpachauri725@gmail.com>
2024-05-05 21:04:07 +0530

    Created BookServiceImpl Class

---
+++ src/main/java/io/github/prajjwal/books/services/impl/BookServiceImpl.java
@@ -0,0 +1,8 @@
+package io.github.prajjwal.books.services.impl;
+
+import io.github.prajjwal.books.services.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+}
